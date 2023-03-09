97413769e6462032b7d60f3b1ba36b6c19d4120c
Antek93 <antonio.savino1993@gmail.com>
2023-03-09 17:08:40 +0100

    creati header manin footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laravel Comics</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -23,8 +23,14 @@
         </style>
     </head>
     <body class="antialiased">
-        <div>
-            ciao
-        </div>
+        <header>
+            
+        </header>
+        <main>
+
+        </main>
+        <footer>
+
+        </footer>
     </body>
 </html>
