18a1356b4e02fa41ced3d97474cde4c2ff020cd4
Antek93 <antonio.savino1993@gmail.com>
2023-03-09 16:34:35 +0100

    added bootstrap+cartella img, removed postCSS

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 import './bootstrap';
+
+import '~resources/scss/app.scss';
+
+import.meta.glob([
+    '../img/**'
+]);
+
+// da riutilizzare nel caso serva su xxx.blade.php:
+
+// <img src="{{ Vite::asset('resources/img/nomefileimmagine.estensioneImmagine') }}" alt="">
